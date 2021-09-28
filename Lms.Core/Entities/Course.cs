ef3c46af3d4431f299886e99175b8c4a1a8a5aef
rosy871 <yalameju@gmail.com>
2021-09-28 18:18:02 +0200

    CourseandModuleEntities

---
+++ Lms.Core/Entities/Course.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Entities
+{
+   public class Course
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public ICollection<Module> Modules { get; set; }
+    }
+}
---
+++ Lms.Core/Entities/Module.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Entities
+{
+   public class Module
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public int CourseId { get; set; }
+        public Course Course { get; set; }
+
+
+    }
+}
