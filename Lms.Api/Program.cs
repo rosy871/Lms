b28c704b331af034284029f7de3b097d6e1d01ba
rosy871 <yalameju@gmail.com>
2021-09-29 10:48:15 +0200

    seeddata

--- Lms.Api/Program.cs
+++ Lms.Api/Program.cs
@@ -1,5 +1,8 @@
+using Lms.Data.Data;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -13,10 +16,28 @@ namespace Lms.Api
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                var context = services.GetRequiredService<LmsApiContext>();
+                context.Database.EnsureDeleted();
+                context.Database.Migrate();
+                try
+                {
+                    SeedData.InitAsync(services,context).Wait();
+                }
+                catch (Exception ex)
+                {
+
+                    throw;
+                }
+            }
+            host.Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
---
+++ Lms.Data/Data/SeedData.cs
@@ -0,0 +1,78 @@
+﻿using Bogus;
+using Lms.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Data.Data
+{
+    public class SeedData
+    {
+        private static LmsApiContext db;
+        private static Faker faker;
+        public static async Task InitAsync(IServiceProvider services, LmsApiContext context)
+        {
+
+            db = context;
+
+            if (await db.Course.AnyAsync()) return;
+
+            faker = new Faker("sv");
+            var courses = GetCourses();
+            await db.AddRangeAsync(courses);
+
+            var modules = GetModules(courses);
+            await db.AddRangeAsync(modules);
+
+            await db.SaveChangesAsync();
+
+        }
+
+        private static IEnumerable<Module> GetModules(IEnumerable<Course> courses)
+        {
+            var modules = new List<Module>();
+
+            foreach (var course in courses)
+            {
+                var randomnum = faker.Random.Int(2, 6);
+                for(int i=0;i<randomnum;i++)
+                {
+                    var module = new Module
+                    {
+                        Course = course,
+                        Title = faker.Company.CatchPhrase(),
+                        StartDate = course.StartDate.AddDays(faker.Random.Int(0, 30))
+
+                    };
+                    modules.Add(module);
+                }
+
+
+            }
+            return modules;
+        }
+
+        private static IEnumerable<Course> GetCourses()
+        {
+            var courses = new List<Course>();
+            for(int i=0; i<20;i++)
+            {
+                var course = new Course
+                {
+                    Title = faker.Company.CatchPhrase(),
+                    StartDate = DateTime.Now.AddDays(faker.Random.Int(-10, 30))
+                };
+                courses.Add(course);
+            }
+
+
+            return courses;
+        }
+
+
+    }
+}
