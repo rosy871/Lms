3534f03214d24c712b9a6a15832a337c0d308d24
rosy871 <yalameju@gmail.com>
2021-09-30 13:41:39 +0200

    Dto

---
+++ Lms.Core/Dto/ModuleDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lms.Core.Dto
+{
+    class ModuleDto
+    {
+        public string Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime StopDate { get { return StartDate.AddMonths(1); } }
+    }
+}
